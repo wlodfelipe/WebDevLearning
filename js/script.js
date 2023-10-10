9fb7e55b2e3ae95b8b986f1f78d26b7d00623d9f
felipe <felipecsena877@gmail.com>
2023-10-10 20:38:59 -0300

    funçao de trocar cor de fundo dinamica

--- js/script.js
+++ js/script.js
@@ -1,51 +1,37 @@
+
+/*adiciona um eventListener que só deixará essas funçoes carregarem quando o DOM
+for completamente carregado, assim evitando das funçoes atropelarem a estrutura do site.
+(da para adicionar outras funçoes que precisam disso aqui tambem, como das colunas)*/
 document.addEventListener("DOMContentLoaded", function () {
-    // Função de chamada para carregar cores no carregamento da página
-    loadColor();
+    const colorInput = document.getElementById('colors');
+    const body = document.querySelector('body');
 
-    // Adicione um evento ao botão "Crocar de Cor"
-    var colorButton = document.getElementById("colorButton");
-    colorButton.addEventListener("click", toggleColorPicker);
+    colorInput.addEventListener('input', () => {
+        const selectedColor = colorInput.value;
+        body.style.backgroundColor = selectedColor;
+        saveBackgroundColor(selectedColor);
+    });
 
-    // Obtenha todos os botões coloridos e adicione um evento a cada um
-    var colorButtons = document.querySelectorAll(".color-option");
-    colorButtons.forEach(function (button) {
-        button.addEventListener("click", function () {
-            var color = button.style.backgroundColor;
-            changeColor(color);
-        });
+    //vai salvar a cor escolhida pelo usuario dentro do localstorage, pegando a propriedade do background-color
+    function saveBackgroundColor(hexadecimalColor) {
+        localStorage.setItem("background-color", hexadecimalColor);
+    }
+
+    // vai pegar a cor salva no local storage(se tiver) e rodar ela toda vez q o site atualizar
+    window.addEventListener('load', () => {
+        const savedColor = localStorage.getItem("background-color");
+        if (savedColor) {
+            body.style.backgroundColor = savedColor;
+            colorInput.value = savedColor;
+        }
     });
 });
 
-function changeColor(color) {
-    document.getElementsByTagName("BODY")[0].style.backgroundColor = color;
-    saveBackgroundColor(color);
-}
-
-function saveBackgroundColor(hexadecimalColor) {
-    localStorage.setItem("background-color", hexadecimalColor);
-}
-
-function loadColor() {
-    var backgroundColor = localStorage.getItem("background-color");
-    if (backgroundColor) {
-        changeColor(backgroundColor);
-    }
-}
-
-function toggleColorPicker() {
-    var colorSelector = document.getElementById("colors");
-    if (colorSelector.style.display === "none" || colorSelector.style.display === "") {
-        colorSelector.style.display = "block";
-    } else {
-        colorSelector.style.display = "none";
-    }
-}
-
-//somente a parte da coluna
+// ----------  somente a parte da coluna ------------------
 function addTask() {
     var taskText = document.getElementById('newTask').value
 
-    if (taskText === ""){
+    if (taskText === "") {
         alert('por favor, isira uma tarefa.')
         return
     }
